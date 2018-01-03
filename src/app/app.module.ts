9e59a6ece37cfb382624861e381fceb5762ea56f
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-03 17:57:53 -0200

    Criando um Feature Module (Módulo de Compra) - Aula 78 completa :D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -17,10 +16,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from 'app/shared/shared.module';
 
@@ -38,9 +34,6 @@ import { SharedModule } from 'app/shared/shared.module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,    
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { Routes } from "@angular/router";
 import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from 'app/restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from 'app/restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from 'app/order/order.component';
 import { OrderSummaryComponent } from 'app/order-summary/order-summary.component';
 
 
@@ -17,7 +16,7 @@ export const ROUTES: Routes = [
             {path:'menu',component:MenuComponent},
             {path:'reviews',component:ReviewsComponent}
         ]},
-    {path:'order',component:OrderComponent},        
+    {path:'order',loadChildren:'./order/order.module#OrderModule'},        
     {path:'order-summary',component:OrderSummaryComponent},
     {path:'about',loadChildren:'./about/about.module#AboutModule'}
 ]
