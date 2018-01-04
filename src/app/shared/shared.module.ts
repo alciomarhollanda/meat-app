9c8f0ee77d71f4d4e97fd6f4ead17f1b0894600d
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-04 16:52:06 -0200

    Aplicando Animações no Snackbar Seção 9, aula 85

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,17 +9,19 @@ import { RatingComponent } from './rating/rating.component';
 import { OrderService } from '../order/order.service';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { RestaurantsService } from '../restaurants/restaurants.service';
+import { SnackbarComponent } from '../shared/messages/snackbar/snackbar.component';
 
 
 
 @NgModule({
-    declarations: [InputComponent, RadioComponent, RatingComponent],
+    declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
 
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
 
     exports: [InputComponent, RadioComponent
         , RatingComponent, CommonModule
-        , FormsModule, ReactiveFormsModule]
+        , FormsModule, ReactiveFormsModule
+        , SnackbarComponent  ]
 })
 export class SharedModule {
 
