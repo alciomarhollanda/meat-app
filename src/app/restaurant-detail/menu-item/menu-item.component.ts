9468130cc04713fba38ba73ced22e470bac8e4af
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-05 15:21:37 -0200

    Animação nos Itens do Menu Seção 9, aula 89

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import {MenuItem} from './menu-item.model'
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('700ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
- @Input() menuItem : MenuItem
- @Output() add = new EventEmitter()
+  menuItemStatus = 'ready';
+
+  @Input() menuItem: MenuItem;
+  @Output() add = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  emitAddEvent(){
+  emitAddEvent() {
 
     this.add.emit(this.menuItem)
-        
+
   }
 
 }
