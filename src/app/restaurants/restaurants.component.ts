e2c802f809749c3d02df53cc0377e49c295d7893
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-07 18:27:06 -0200

    Operadores debounceTime e distinctUntilChanged Seção 12, aula 105

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -6,6 +6,9 @@ import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './restaurant/restaurant.model';
 
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'mt-restaurants',
@@ -42,9 +45,11 @@ export class RestaurantsComponent implements OnInit {
     });
 
     this.searchControl.valueChanges
+      .debounceTime(500)
+      .do(searchTerm => console.log('q=${' + searchTerm + '}'))
       .switchMap(searchTerm =>
         this.RestaurantsService.restaurants(searchTerm))
-      .subscribe(restaurants => this.restaurants = restaurants)
+      .subscribe(restaurants => this.restaurants = restaurants);
 
     this.RestaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants);
