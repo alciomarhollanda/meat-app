b65053317d33a6ea3796f953fdf25ea6df09984c
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-07 17:10:50 -0200

    Aplicando Animações na Barra de Busca Seção 12, aula 103

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,19 +1,40 @@
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        'max-height': '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        'max-height': '70px',
+        'margin-top': '20px'
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[]
+  searchBarState = 'hidden';
+
+  restaurants: Restaurant[];
 
   constructor(private RestaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-     this.RestaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
+    this.RestaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
