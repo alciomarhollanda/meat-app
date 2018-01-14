b4bfca7e68a3aca4b483ca67620b08fd030a61ce
Alciomar Hollanda <alciomarhollanda@gmail.com>
2018-01-14 16:48:47 -0200

    Criando um Error Handler Global Seção 16, aula 126

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -2,18 +2,34 @@
 
 import { Observable } from 'rxjs/Observable';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { NotificationService } from './shared/messages/notification.service'
+import { LoginService } from 'app/security/login/login.service';
 
-export class ErrorHandler {
-    static handleError(error: HttpErrorResponse | any) {
-        let errorMenssage: string;
 
-        if (error instanceof HttpErrorResponse) {
-            errorMenssage = 'Erro ' + error.status + ' ao acessar a URL ' + error.url + ' - ' + error.statusText + ' - ' + error.error;
-        } else {
-            errorMenssage = error.toString();
+
+@Injectable()
+export class ApplicationErrorHandler extends ErrorHandler {
+
+
+    constructor(private ns: NotificationService, private injector: Injector) {
+        super();
+    }
+    handleError(errorResponse: HttpErrorResponse | any) {
+        if (errorResponse instanceof HttpErrorResponse) {
+            const message = errorResponse.error.message
+            switch (errorResponse.status) {
+                case 401:
+                    this.injector.get(LoginService).handleLogin();
+                    break;
+                case 403:
+                    this.ns.notify(message || 'Não Autorizado.');
+                    break;
+                case 404:
+                    this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes');
+                    break;
+            }
         }
-
-        console.log(errorMenssage)
-        return Observable.throw(errorMenssage)
+        super.handleError(errorResponse)
     }
 }
